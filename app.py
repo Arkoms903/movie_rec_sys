6291caf6635b831734142cda6a07ad2e35548322
Arko <arkobhattacharyya05.com~>
2026-03-07 18:47:12 +0530

    added similarity.pkl and modified app.py

--- app.py
+++ app.py
@@ -2,6 +2,18 @@ import streamlit as st
 import pickle
 import requests
 import pandas as pd
+import numpy as np
+
+similarity = pickle.load(open("similarity.pkl", "rb"))
+
+top_k = 50
+top_similar_movies = []
+
+for i in range(len(similarity)):
+    similar_indices = np.argsort(similarity[i])[-top_k-1:-1]
+    top_similar_movies.append(similar_indices.tolist())  # important
+
+pickle.dump(top_similar_movies, open("similarity.pkl", "wb"))
 
 st.title('Movie Recommendation System')
 session = requests.Session()
